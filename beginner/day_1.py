f58121c6e6b8efe23af22f35e7c1fcb9a87040bf
himanshu97 <himanshu@wizikey.com>
2022-01-17 21:30:02 +0530

    Day 1

---
+++ beginner/day_1.py
@@ -0,0 +1,19 @@
+#print("Hello World")
+
+# name = input("Enter Your Name: ")
+# print(len(name))
+
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print(f"a: {a}")
+# print(f"b: {b}")
+print("Welcome to Band Name Generator...!!!")
+name = input("Enter your Fav. City Name: \n")
+pet_name = input("Enter your Pet Name: \n")
+
+print(f"Suggested Brand Name: {name} {pet_name}")
